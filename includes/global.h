4cb237002fcb15f16b19c1a25771ab4e9b1b6dfa
Corry TERBLANCHE <cterblan@c5r10s3.wethinkcode.co.za>
2019-07-19 15:34:15 +0200

    norm

--- includes/global.h
+++ includes/global.h
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   global.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cterblan <cterblan@students.wethinkcode    +#+  +:+       +#+        */
+/*   By: cterblan <cterblan@student>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 15:54:01 by cterblan          #+#    #+#             */
-/*   Updated: 2019/07/17 15:54:03 by cterblan         ###   ########.fr       */
+/*   Updated: 2019/07/19 15:33:44 by cterblan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GLOBAL_H
-	#define GLOBAL_H
+# define GLOBAL_H
 
-	#include "./malloc.h"
-	t_zone 	*s_base = NULL;
+# include "./malloc.h"
+
+t_zone *g_base = NULL;
 
 #endif
--- includes/malloc.h
+++ includes/malloc.h
@@ -3,84 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   malloc.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cterblan <cterblan@students.wethinkcode    +#+  +:+       +#+        */
+/*   By: cterblan <cterblan@student>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 11:45:36 by cterblan          #+#    #+#             */
-/*   Updated: 2019/07/19 12:59:13 by cterblan         ###   ########.fr       */
+/*   Updated: 2019/07/19 15:32:39 by cterblan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-**  References
-**  https://medium.com/a-42-journey/how-to-create-your-own-malloc-library-b86fedd39b96
-**  https://danluu.com/malloc-tutorial/
-*/
-
 #ifndef MALLOC_H
-	#define MALLOC_H
+# define MALLOC_H
 
-	/*
-	** Includes
-	*/
-	#include "../lib/libft/inc/ft_printf.h"
-	#include <unistd.h>
-	#include <sys/mman.h>
+/*
+** Includes
+*/
+# include "../lib/libft/inc/ft_printf.h"
+# include <unistd.h>
+# include <sys/mman.h>
 
+/*
+** Defines
+*/
+typedef enum	e_type{
+	TINY = 0,
+	SMALL = 1,
+	LARGE = 2
+}				t_type;
 
+typedef struct				s_block{
+	void					*data;
+	size_t					size;
+	struct s_block			*next;
+	struct s_block			*previous;
+}							t_block;
 
-	/*
-	** Defines
-	*/
-	typedef enum	e_type{
-		TINY = 0,
-		SMALL = 1,
-		LARGE = 2
-	}				t_type;
+typedef struct				s_zone{
+	t_type					type;
+	size_t					size;
+	t_block					*block_start;
+	void					*data;
+	struct s_zone			*next;
+	struct s_zone			*previous;
+}							t_zone;
 
-	typedef struct				s_block{
-		void					*data;
-		size_t					size;
-		struct s_block			*next;
-		struct s_block			*previous;
-	}							t_block;
+# define HEAP_META_SIZE sizeof(t_zone)
+# define BLOCK_META_SIZE sizeof(t_zone)
+# define ZONE_SIZE getpagesize()
+# define TINY_SIZE 32
+# define SMALL_SIZE 128
+# define FIT_ZONE(S) (((S) / ZONE_SIZE) + 1) * ZONE_SIZE
+# define TINY_ZONE FIT_ZONE(((TINY_SIZE+BLOCK_META_SIZE)*100)+HEAP_META_SIZE)
+# define SMALL_ZONE FIT_ZONE(((SMALL_SIZE+BLOCK_META_SIZE)*100)+HEAP_META_SIZE)
 
-	typedef struct              s_zone{
-		t_type					type;
-		size_t					size;
-		t_block					*block_start;
-		void					*data;
-		struct s_zone           *next;
-		struct s_zone           *previous;
-	}                           t_zone;
+/*
+** Global Variables
+*/
+# ifndef BASE_MALLOC
 
+extern t_zone *g_base;
+# endif
 
-	#define						HEAP_META_SIZE sizeof(t_zone)
-	#define						BLOCK_META_SIZE sizeof(t_zone)
-	#define 					ZONE_SIZE getpagesize()
-	#define 					TINY_SIZE 32
-	#define 					SMALL_SIZE 128
-	#define 					FIT_ZONE(S) (((S) / ZONE_SIZE) + 1) * ZONE_SIZE
-	#define 					TINY_ZONE FIT_ZONE(((TINY_SIZE + BLOCK_META_SIZE) * 100) + HEAP_META_SIZE)
-	#define 					SMALL_ZONE FIT_ZONE(((SMALL_SIZE + BLOCK_META_SIZE) * 100) + HEAP_META_SIZE)
-	/*
-	** Global Variables
-	*/
-	#ifndef BASE_MALLOC
-		extern t_zone *s_base;
-	#endif
-	/*
-	 ** Declarations
-	 */
-	void					    *ft_malloc(size_t size);
-	void					    show_alloc_mem();
-	void					    ft_free(void *ptr);
-	void					    *ft_realloc(void *ptr, size_t size);
+/*
+** Declarations
+*/
+void						*ft_malloc(size_t size);
+void						show_alloc_mem();
+void						ft_free(void *ptr);
+void						*ft_realloc(void *ptr, size_t size);
 
-	t_zone						*zone_find(size_t size);
-	t_zone						*zone_new(size_t size);
-	t_type						zone_type(size_t size);
-	int							zone_free(t_zone *zone);
-	t_block						*block_new(t_zone *zone, size_t size);
-	void						block_free(t_zone *zone,t_block *block);
+t_zone						*zone_find(size_t size);
+t_zone						*zone_new(size_t size);
+t_type						zone_type(size_t size);
+int							zone_free(t_zone *zone);
+t_block						*block_new(t_zone *zone, size_t size);
+void						block_free(t_zone *zone, t_block *block);
 
 #endif
